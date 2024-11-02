2d46cac3a7b0f648522b42c7cbf23d017441b7b8
Rita B <margarita.bern@gmail.com>
2024-11-02 00:51:08 -0400

    Added Dictionary js and css
    Added 2 new components and created a useState in the js file.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./logo-shecodes.png";
-import './App.css';
+import './App.css';             
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-       <button type="button" className="btn btn-success"> Hello</button>
-      </header>
+      <div className="container"> 
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by SheCodes
+            </small>
+        </footer>
+      </div>
     </div>
   );
 }
